62235b8057adc3b7cf11938596a6927ff807bb52
Sheilim <sheilim@Sheilims-MacBook-Pro.local>
2020-12-26 14:58:29 -0600

    Inicializacion

---
+++ README.md
@@ -0,0 +1,1 @@
+# shimli-sdk
---
+++ index.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+
+function connectShimli(url, json){
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(json);
+        var config = {
+            method: 'post',
+            url: url,
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            data : data
+        };
+    
+        axios(config)
+        .then(function (response) {
+            resolve({
+                data: response.data
+            })
+        })
+        .catch(function (error) {
+            resolve({
+                error: true,
+                txt: error
+            })
+        });
+    })
+}
+
+module.export = {
+    config: {},
+    sendMessage: (type, body, to, instance, caption = null, lat = null, lng = null) => {
+        return new Promise(async (resolve, reject) => {
+            if(!config.token){
+                resolve({
+                    error: true,
+                    txt: 'Configurate Token'
+                })
+            }
+            let data_ = {
+                "token": config.token,
+                "type": type, 
+                "body": body, 
+                "to": to,
+                "instance": instance
+            };
+            if(caption)
+                data_['caption'] = caption;
+            if(filename)
+                data_['filename'] = filename;
+            if(lat)
+                data_['lat'] = lat;
+            if(lng)
+                data_['lng'] = lng;
+            const result = await connectShimli(
+                'https://api.shimli.app/v1/msg-wh',
+                data_
+            );
+            resolve(result);
+        })
+    }
+}
